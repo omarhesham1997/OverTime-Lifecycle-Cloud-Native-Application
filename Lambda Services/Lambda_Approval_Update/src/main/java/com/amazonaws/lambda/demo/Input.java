6feff055f3e1be46a895e9397a45fa49de97ae70
Knight22x <52579262+Knight22x@users.noreply.github.com>
2019-07-29 13:43:16 +0200

    update

--- Lambda Services/Lambda_Approval_Update/src/main/java/com/amazonaws/lambda/demo/Input.java
+++ Lambda Services/Lambda_Approval_Update/src/main/java/com/amazonaws/lambda/demo/Input.java
@@ -3,12 +3,26 @@ package com.amazonaws.lambda.demo;
 public class Input {
 	String month;
 	String year;
-	int empid;
+	String empid;
+	String choice;
 	public String getMonth() {
 		return month;
 	}
 	public void setMonth(String month) {
 		this.month = month;
+	}
+	
+	public String getEmpid() {
+		return empid;
+	}
+	public void setEmpid(String empid) {
+		this.empid = empid;
+	}
+	public String getChoice() {
+		return choice;
+	}
+	public void setChoice(String choice) {
+		this.choice = choice;
 	}
 	public String getYear() {
 		return year;
@@ -16,12 +30,7 @@ public class Input {
 	public void setYear(String year) {
 		this.year = year;
 	}
-	public int getEmpid() {
-		return empid;
-	}
-	public void setEmpid(int empid) {
-		this.empid = empid;
-	}
+
 	
 
 }
---
+++ Lambda Services/Lambda_Approval_Update/src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
@@ -0,0 +1,57 @@
+package com.amazonaws.lambda.demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaFunctionHandler implements RequestHandler<Input, String> {
+
+	@Override
+	public String handleRequest(Input i, Context context) {
+		context.getLogger().log("Input: " + i);
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		String host_name=System.getenv("host_name");
+		String user_name=System.getenv("user_name");
+		String password=System.getenv("password");
+		String dbname=System.getenv("dbname");
+		Connection connect;
+		try {
+			connect = DriverManager.getConnection(  
+					"jdbc:mysql://" +host_name+":3306/"+dbname,user_name,password);
+
+			String query="UPDATE overtime SET status ='"+i.getChoice()+ "' where emp_id = '" +i.getEmpid()+ "' AND month = '"+i.getMonth()+"' AND year = '"+i.getYear()+"'" ;
+			PreparedStatement preparedStmt = connect.prepareStatement(query);
+			preparedStmt.executeUpdate();
+			connect.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		
+		
+		
+		
+		return "Success";
+	}
+
+}
